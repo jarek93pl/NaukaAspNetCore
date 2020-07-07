cb82c762efcc40963a1857dc8333897435b63a65
Jarosław Gosławski <jarek93pl@hotmail.com>
2020-07-07 09:41:19 +0200

    Dodanie obszarów

---
+++ Areas/Areas/Con2Area/Controllers/Default2Controller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Areas.Areas.Con2Area.Controllers
+{
+    public class Default2Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
