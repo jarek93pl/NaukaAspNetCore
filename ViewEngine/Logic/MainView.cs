fe63bb4b7678ad1111ec6b5e08638059bbbf6ff6
Jarosław Gosławski <jarek93pl@hotmail.com>
2020-07-11 14:25:02 +0200

    Dodanie VeiwEngine-a

---
+++ ViewEngine/Logic/MainView.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewEngine.Logic
+{
+    public class MainView : IView
+    {
+        public string Path => string.Empty;
+
+        public Task RenderAsync(ViewContext context)
+        {
+            return Task.Run(() =>
+            {
+                StringBuilder sn = new StringBuilder();
+                foreach (var item in context.ViewData)
+                {
+                    sn.AppendLine($"{item.Key}: {item.Value}");
+                }
+                context.HttpContext.Response.WriteAsync(sn.ToString());
+            });
+        }
+    }
+}
---
+++ ViewEngine/Logic/MainViewEngine.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ViewEngine.Logic
+{
+    public class MainViewEngine : IViewEngine
+    {
+        public ViewEngineResult FindView(ActionContext context, string viewName, bool isMainPage)
+        {
+            return ViewEngineResult.Found(viewName, new MainView());
+        }
+
+        public ViewEngineResult GetView(string executingFilePath, string viewPath, bool isMainPage)
+        {
+            return ViewEngineResult.Found("vidok", new MainView());
+        }
+    }
+}
