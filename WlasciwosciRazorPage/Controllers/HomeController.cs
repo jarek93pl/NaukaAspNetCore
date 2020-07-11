1e3a9ec43451476576809131a93c6679a08b940d
Jarosław Gosławski <jarek93pl@hotmail.com>
2020-07-11 14:57:00 +0200

    Właściwości razor  + sekcje i widoki częściowe

---
+++ WlasciwosciRazorPage/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
+using WlasciwosciRazorPage.Models;
+
+namespace WlasciwosciRazorPage.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            TempData["Dar"] = "dziwny text";
+            return View();
+        }
+    }
+}
