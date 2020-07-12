4f384313f96a6dd3b8001ff6c19d65153b4f6c26
Jarosław Gosławski <jarek93pl@hotmail.com>
2020-07-12 12:21:40 +0200

    Dodanie location Expander

---
+++ LocationExpander/Logic/ViewLocationExpander.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocationExpander.Logic
+{
+    public class ViewLocationExpander : IViewLocationExpander
+    {
+        public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
+        {
+            foreach (var item in viewLocations)
+            {
+                System.Diagnostics.Debug.WriteLine(item);
+            }
+            return viewLocations;
+        }
+
+        public void PopulateValues(ViewLocationExpanderContext context)
+        {
+        }
+    }
+}
