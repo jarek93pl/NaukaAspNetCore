d77e1bad8ea8bc9dc7fd3c40a72656f4bf0133b1
Jarosław Gosławski <jarek93pl@hotmail.com>
2020-07-12 12:57:26 +0200

    ViewComponent

---
+++ komponentWidoku/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using komponentWidoku.Models;
+
+namespace komponentWidoku.Controllers
+{
+    [ViewComponentAttribute(Name = "X")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string Invoke() => "text";
+    }
+}
---
+++ komponentWidoku/Controllers/PocoViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace komponentWidoku.Controllers
+{
+    public class PocoViewComponent
+    {
+        public string Invoke()
+        {
+            return "<p> tekst z PocoViewComponent, znaki spoecjalne nie przerobiły się na html</p>";
+        }
+    }
+}
