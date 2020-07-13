72db32471eea97062fecb81ff4769f88c1794ce5
Jarosław Gosławski <jarek93pl@hotmail.com>
2020-07-13 12:08:06 +0200

    Dodanie klasy EmailTagHelper

---
+++ AuthoringTagHelpers/TagHelper/EmailTagHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+namespace AuthoringTagHelpers.TagHelpers
+{
+    [HtmlTargetElement("email", TagStructure = TagStructure.WithoutEndTag)]
+
+    public class EmailTagHelper : TagHelper
+    {
+        private const string EmailDomain = "contoso.com";
+
+        // Can be passed via <email mail-to="..." />. 
+        // PascalCase gets translated into kebab-case.
+        public string v { get; set; }// v jest pobierana mimo że nie jest uzupeniona
+
+        public override async void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";                                 // Replaces <email> with <a> tag
+            var target = output.Content.GetContent() + "@" + EmailDomain;
+            output.Attributes.SetAttribute("href", "mailto:" + target);
+            output.Content.SetContent(target);
+        }
+    }
+}
