29ff2c888c30d1696c46c3c4a79795c777e75026
Jarosław Gosławski <jarek93pl@hotmail.com>
2020-07-14 10:41:00 +0200

    Atrybuty optymalizacji i versionowania v1

---
+++ AtrybutyOptymalizacjaIVersionowanie/Controllers/AtrybutycssController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AtrybutyOptymalizacjaIVersionowanie.Controllers
+{
+    public class AtrybutycssController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AtrybutyOptymalizacjaIVersionowanie/wwwroot/js/scrA.js
@@ -0,0 +1,1 @@
+﻿alert('wiadomośc z skryptu a')
