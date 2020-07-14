8bf4669a0767cc34133cf4720309c3591cb9e4d4
Jarosław Gosławski <jarek93pl@hotmail.com>
2020-07-14 14:17:49 +0200

    Dodanie ceshowania zapytania

---
+++ AtrybutyOptymalizacjaIVersionowanie/Controllers/CecheController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace AtrybutyOptymalizacjaIVersionowanie.Controllers
+{
+    public class CecheController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Person(int id)
+        {
+            return View(id);
+        }
+    }
+}
